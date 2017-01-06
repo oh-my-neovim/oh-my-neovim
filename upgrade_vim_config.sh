8a7a7aad78b7462bd867efb449e9ac9a61e4c6ec
yacut <yacut@users.noreply.github.com>
2017-01-06 08:31:54 +0100

    Update upgrade_vim_config.sh

--- upgrade_vim_config.sh
+++ upgrade_vim_config.sh
@@ -51,10 +51,12 @@ if [ ! -f ~/.config/nvim/init.vim ] || [ ! -h ~/.config/nvim/init.vim ]; then
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
-  cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf;
+  printf "${BLUE}Downloading "Sauce Code Pro Nerd Font Complete.ttf" to "~/Library/Fonts" folder...${NORMAL}\n"
+  cd ~/Library/Fonts && curl -fLo "Sauce Code Pro Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf;
 else
+  printf "${BLUE}Downloading "Sauce Code Pro Nerd Font Complete.ttf" to "~/.local/share/fonts" folder...${NORMAL}\n"
   mkdir -p ~/.local/share/fonts;
-  cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf;
+  cd ~/.local/share/fonts && curl -fLo "Sauce Code Pro Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf;
 fi
 
 printf "${BLUE}Updating plugins...${NORMAL}\n"
