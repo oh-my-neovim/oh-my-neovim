ff6b5954aa5a55829973142f4ed39e5df780afa6
yacut <yacut@users.noreply.github.com>
2017-01-06 08:52:49 +0100

    Update upgrade_vim_config.sh

--- upgrade_vim_config.sh
+++ upgrade_vim_config.sh
@@ -52,15 +52,15 @@ fi
 
 OSTYPE=$(uname)
 if [ "$OSTYPE" == "Darwin" ]; then
-  if [ ! -f ~/Library/Fonts/Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf ]; then
-    printf "${BLUE}Downloading Sauce Code Pro Nerd Font Complete.ttf to ~/Library/Fonts folder...${NORMAL}\n"
-    cd ~/Library/Fonts && curl -fLo "Sauce Code Pro Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf;
+  if [ ! -f ~/Library/Fonts/SauceCodePro\ Nerd\ Font.ttf ]; then
+    printf "${BLUE}Downloading SauceCodePro Nerd Font.ttf to ~/Library/Fonts folder...${NORMAL}\n"
+    cd ~/Library/Fonts && curl -fLo "SauceCodePro Nerd Font.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf;
   fi
 else
-  if [ ! -f ~/.local/share/fonts/Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf ]; then
-    printf "${BLUE}Downloading Sauce Code Pro Nerd Font Complete.ttf to ~/.local/share/fonts folder...${NORMAL}\n"
+  if [ ! -f ~/.local/share/fonts/SauceCodePro\ Nerd\ Font.ttf ]; then
+    printf "${BLUE}Downloading SauceCodePro Nerd Font.ttf to ~/.local/share/fonts folder...${NORMAL}\n"
     mkdir -p ~/.local/share/fonts;
-    cd ~/.local/share/fonts && curl -fLo "Sauce Code Pro Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf;
+    cd ~/.local/share/fonts && curl -fLo "SauceCodePro Nerd Font.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf;
   fi
 fi
 
