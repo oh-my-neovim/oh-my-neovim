6c852301a1898cc833389c5af23047a54de14e83
yacut <yacut@users.noreply.github.com>
2017-01-10 14:36:04 +0100

    Update upgrade_vim_config.sh

--- upgrade_vim_config.sh
+++ upgrade_vim_config.sh
@@ -81,11 +81,16 @@ if [ "$OS_TYPE" = Linux ]; then
       if [ ! -f /etc/apt/sources.list.d/nodesource.list ]; then
         curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash 
       fi
-      sudo apt-get install -y -q nodejs editorconfig silversearcher-ag libxml2-utils python-pip;
+      if [ ! -f /etc/apt/sources.list.d/yarn.list ]; then
+        curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -;
+        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list;
+      fi
+      sudo apt-get install -y -q nodejs yarn editorconfig silversearcher-ag libxml2-utils python-pip;
   elif [ -f /etc/redhat-release ]; then
       printf "${BLUE}Installing nodejs...${NORMAL}\n";
       curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-      sudo yum -y install nodejs the_silver_searcher python-pip;
+      sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+      sudo yum -y install nodejs yarn the_silver_searcher python-pip;
   fi
   sudo pip install yamllint ansible-lint;
   
