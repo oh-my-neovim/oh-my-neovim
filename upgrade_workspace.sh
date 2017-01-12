090702769e75188693c8ba72d6ac1ec52f092be9
yacut <yacut@protonmail.com>
2017-01-12 21:24:03 +0100

    refactoring

--- upgrade_workspace.sh
+++ upgrade_workspace.sh
@@ -37,7 +37,7 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
 fi
 
 printf "${BLUE}Downloading .vimrc gist file from github...${NORMAL}\n"
-curl -o ~/.vimrc -L https://raw.githubusercontent.com/yacut/workspace/master/.vimrc
+curl -sfLo ~/.vimrc https://raw.githubusercontent.com/yacut/workspace/master/.vimrc
 printf "${GREEN}Vim config upgraded!${NORMAL}\n"
 
 printf "${BLUE}Checking neovim links${NORMAL}\n"
@@ -59,9 +59,10 @@ if [ "$OS_TYPE" = Darwin ]; then
       unzip -o SourceCodePro.zip && rm SourceCodePro.zip;
   fi
 
-  printf "${BLUE}Installing dependencies...${NORMAL}\n";
-  brew install node yarn editorconfig the_silver_searcher libxml2 python python3;
-  pip install --quiet yamllint ansible-lint;
+  printf "${BLUE}Installing dependencies with brew...${NORMAL}\n";
+  brew install node yarn editorconfig the_silver_searcher libxml2 python python3 ruby;
+  printf "${BLUE}Upgrading linting tools with pip...${NORMAL}\n";
+  pip install --quiet --user --upgrade yamllint ansible-lint;
 
   printf "${BLUE}Updating global npm packages...${NORMAL}\n";
   yarn global upgrade tern eslint jsonlint babel-eslint eslint-plugin-react;
@@ -76,8 +77,8 @@ if [ "$OS_TYPE" = Linux ]; then
       unzip -o SourceCodePro.zip && rm SourceCodePro.zip;
   fi
 
-  printf "${BLUE}Installing dependencies...${NORMAL}\n";
   if [ -f /etc/debian_version ]; then
+      printf "${BLUE}Installing dependencies with apt - ${RED}sudo privileges required...${NORMAL}\n";
       if [ ! -f /etc/apt/sources.list.d/nodesource.list ]; then
         curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
       fi
@@ -85,28 +86,36 @@ if [ "$OS_TYPE" = Linux ]; then
         curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -;
         echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list;
       fi
-      sudo apt-get install -y -q nodejs yarn editorconfig silversearcher-ag libxml2-utils python-pip;
+      sudo apt-get install -y -q nodejs yarn editorconfig silversearcher-ag libxml2-utils python python3 python-pip xsel ruby ruby-dev build-essential libssl-dev libffi-dev python-dev;
   elif [ -f /etc/redhat-release ]; then
-      printf "${BLUE}Installing nodejs...${NORMAL}\n";
+      printf "${BLUE}Installing dependencies with yum - ${RED}sudo privileges required...${NORMAL}\n";
       curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
       sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-      sudo yum -y install nodejs yarn the_silver_searcher python-pip;
+      sudo yum -y install nodejs yarn the_silver_searcher python-pip ruby ruby-devel gcc libffi-devel python-devel openssl-devel;
   fi
-  sudo pip install --quiet yamllint ansible-lint;
+  printf "${BLUE}Upgrading lint tools with pip...${NORMAL}\n";
+  pip install --quiet --user yamllint ansible-lint;
 
-  printf "${BLUE}Updating global npm packages...${NORMAL}\n";
+  printf "${BLUE}Updating global npm packages - ${RED}sudo privileges required...${NORMAL}\n";
   sudo yarn global upgrade tern eslint jsonlint babel-eslint eslint-plugin-react;
 fi
 
 printf "${BLUE}Getting Oh My Zsh...${NORMAL}\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+printf "${BLUE}Setting Zsh plugins, theme and PATH...${NORMAL}\n"
 if [ "$OS_TYPE" = Darwin ]; then
   sed -i "" "s/ZSH_THEME=\".*/ZSH_THEME=\"agnoster\"/g" ~/.zshrc
-  sed -i "" "s/plugins=(.*/plugins=(git node npm yarn gulp docker docker-compose kubectl pip brew debian)/g" ~/.zshrc
+  sed -i "" "s/plugins=(.*/plugins=(git node npm yarn gulp docker docker-compose kubectl pip gem brew debian)/g" ~/.zshrc
 else
   sed -i "s/ZSH_THEME=\".*/ZSH_THEME=\"agnoster\"/g" ~/.zshrc
-  sed -i "s/plugins=(.*/plugins=(git node npm yarn gulp docker docker-compose kubectl pip brew debian)/g" ~/.zshrc
+  sed -i "s/plugins=(.*/plugins=(git node npm yarn gulp docker docker-compose kubectl pip gem brew debian)/g" ~/.zshrc
 fi
+grep -q "PATH=\"$(ruby -e 'print Gem.user_dir')/bin:$PATH\"" ~/.zshrc || echo "PATH=\"$(ruby -e 'print Gem.user_dir')/bin:\$PATH\"" >> ~/.zshrc
+
+printf "${BLUE}Updating neovim providers...${NORMAL}\n"
+pip2 install --quiet --user --upgrade neovim
+pip3 install --quiet --user --upgrade neovim
+PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH" gem install --user-install neovim
 
 printf "${BLUE}Updating plugins...${NORMAL}\n"
 hash nvim >/dev/null 2>&1 || {
