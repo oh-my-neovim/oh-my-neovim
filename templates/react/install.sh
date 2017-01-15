bebe195ca9dc1ce9a16426c9fedbea7176557e54
yacut <yacut@protonmail.com>
2017-01-15 22:10:03 +0100

    add react scripts

---
+++ templates/react/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# install npm global packages with yarn
+mkdir -p ~/.yarn-global
+yarn config set prefix ~/.yarn-global
+npm config set prefix ~/.yarn-global
+CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$CURRENT_SHELL" = "zsh" ]; then
+  grep -q "PATH=\"$HOME/.yarn-global/bin:\$PATH\"" ~/.zshrc || echo "PATH=\"$HOME/.yarn-global/bin:\$PATH\"" >> ~/.zshrc
+else
+  grep -q "PATH=\"$HOME/.yarn-global/bin:\$PATH\"" ~/.profile || echo "PATH=\"$HOME/.yarn-global/bin:\$PATH\"" >> ~/.profile
+fi
+yarn global add eslint babel-eslint eslint-plugin-react --global-folder ~/.yarn-global
---
+++ templates/react/uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# uninstall npm global packages with yarn
+yarn global remove eslint babel-eslint eslint-plugin-react --global-folder ~/.yarn-global
