d7ea2ea4cde5067cf2c7fd838ee67cbc21dd5ec1
yacut <yacut@protonmail.com>
2017-01-16 08:52:14 +0100

    install dependencies first

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -24,8 +24,8 @@ printf "${BLUE}%s${NORMAL}\n" "Updating Oh My Neovim"
 cd "$OH_MY_NEOVIM"
 if git pull --rebase --stat origin master
 then
-  env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_PLUGINS="$OH_MY_NEOVIM_PLUGINS" sh "$OH_MY_NEOVIM/tools/install_plugins.sh"
   env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_PLUGINS="$OH_MY_NEOVIM_PLUGINS" sh "$OH_MY_NEOVIM/tools/install_plugin_dependencies.sh"
+  env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_PLUGINS="$OH_MY_NEOVIM_PLUGINS" sh "$OH_MY_NEOVIM/tools/install_plugins.sh"
   printf "${BLUE}%s${NORMAL}\n" "Hooray! Oh My Neovim has been updated and/or is at the current version. Please restart your neovim!"
 else
   printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
