2723da163813d77111d800b6f07db2944e22b6c9
yacut <yacut@protonmail.com>
2017-01-19 22:48:14 +0100

    adjust upgrade script

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -8,7 +8,7 @@ then
   env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_PLUGINS="$OH_MY_NEOVIM_PLUGINS" sh "$OH_MY_NEOVIM/tools/install_plugin_dependencies.sh"
   env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_PLUGINS="$OH_MY_NEOVIM_PLUGINS" sh "$OH_MY_NEOVIM/tools/install_plugins.sh"
   print_logo
-  printf "${BLUE}%s${NORMAL}\n" "Hooray! Oh My Neovim has been updated and/or is at the current version. Please restart your neovim!"
+  printf "${BLUE}%s${NORMAL}\n" "Oh My Neovim has been updated. Please restart your neovim!"
 else
-  printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
+  printf "${RED}%s${NORMAL}\n"
 fi
