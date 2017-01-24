93ac0c8732032ced643c2744e73cfb9ee023d690
yacut <yacut@protonmail.com>
2017-01-24 01:10:46 +0100

    add create dotfiles functions

--- tools/functions.sh
+++ tools/functions.sh
@@ -17,3 +17,35 @@ uninstall_oh_my_neovim () {
 select_plugins_oh_my_neovim () {
   env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_PLUGINS="$OH_MY_NEOVIM_PLUGINS" sh $OH_MY_NEOVIM/tools/select_plugins.sh
 }
+
+create_dotfiles_nodejs () {
+  printf "Createing .editorconfig...\n"
+  cp $OH_MY_NEOVIM/.editorconfig .
+
+  printf "Creating .eslintrc...${NORMAL}\n"
+  cp $OH_MY_NEOVIM/plugins/nodejs/.eslintrc.json .
+
+  printf "Creating .tern-project...${NORMAL}\n"
+  cp $OH_MY_NEOVIM/plugins/javascript/.tern-project .
+
+  printf "Creating .jsbeautifyrc...${NORMAL}\n"
+  cp $OH_MY_NEOVIM/plugins/javascript/.jsbeautifyrc .
+
+  printf "Dotfiles for nodejs project are created!\n"
+}
+
+create_dotfiles_reactjs () {
+  printf "Createing .editorconfig...\n"
+  cp $OH_MY_NEOVIM/.editorconfig .
+
+  printf "Creating .eslintrc...${NORMAL}\n"
+  cp $OH_MY_NEOVIM/plugins/react/.eslintrc.json .
+
+  printf "Creating .tern-project...${NORMAL}\n"
+  cp $OH_MY_NEOVIM/plugins/javascript/.tern-project .
+
+  printf "Creating .jsbeautifyrc...${NORMAL}\n"
+  cp $OH_MY_NEOVIM/plugins/javascript/.jsbeautifyrc .
+
+  printf "Dotfiles for nodejs project are created!\n"
+}
