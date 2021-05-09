7aed5658abfd88775e7b2f1fb2bae3d7a9083506
yacut <roman.frey2020@gmail.com>
2021-05-09 10:22:46 +0200

    upgrade nerd font
    Signed-off-by: yacut <roman.frey2020@gmail.com>

--- extensions/default/install.sh
+++ extensions/default/install.sh
@@ -2,18 +2,20 @@
 OS_TYPE=$(uname)
 if [ "$OS_TYPE" = Darwin ]; then
   # install font
+  rm ~/Library/Fonts/Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf || true
   if [ ! -f ~/Library/Fonts/Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf ]; then
     cd ~/Library/Fonts && \
-      curl -sfLo "SourceCodePro.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v1.0.0/SourceCodePro.zip && \
+      curl -sfLo "SourceCodePro.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip && \
       unzip -o SourceCodePro.zip && rm SourceCodePro.zip
   fi
 elif [ "$OS_TYPE" = Linux ]; then
   # install font
+  rm ~/.local/share/fonts/Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf || true
   if [ ! -f ~/.local/share/fonts/Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf ]; then
     printf "${BLUE}Downloading Sauce Code Pro Nerd Fonts to ~/.local/share/fonts folder...${NORMAL}\n"
     mkdir -p ~/.local/share/fonts
     cd ~/.local/share/fonts && \
-      curl -sfLo "SourceCodePro.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v1.0.0/SourceCodePro.zip && \
+      curl -sfLo "SourceCodePro.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip && \
       unzip -o SourceCodePro.zip && rm SourceCodePro.zip
   fi
 fi
