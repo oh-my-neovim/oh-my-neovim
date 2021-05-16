924467d94ce7b21409c2bc7ea937ca3244b0d589
yacut <roman.frey2020@gmail.com>
2021-05-16 11:18:20 +0200

    refactor
    Signed-off-by: yacut <roman.frey2020@gmail.com>

--- tools/install_extensions.sh
+++ tools/install_extensions.sh
@@ -2,14 +2,21 @@
 . $OH_MY_NEOVIM/tools/custom.sh
 
 printf "${BLUE}Updating Neovim plugins...${NORMAL}\n"
-env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_EXTENSIONS="$OH_MY_NEOVIM_EXTENSIONS" nvim -c ":PlugUpdate" -c ":qa!" --headless || {
+env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_EXTENSIONS="$OH_MY_NEOVIM_EXTENSIONS" nvim -c ':PlugUpdate' -c ':qa!' --headless || {
   printf "Error: Update plugins failed\n"
   printf "Please start nvim and run ':PlugUpdate' manually\n"
 }
 
-env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_EXTENSIONS="$OH_MY_NEOVIM_EXTENSIONS" nvim -c ":UpdateRemotePlugins" -c ":qa!" --headless || {
+env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_EXTENSIONS="$OH_MY_NEOVIM_EXTENSIONS" nvim -c ':UpdateRemotePlugins' -c ':qa!' --headless || {
   printf "Error: Update remote plugins failed\n"
   printf "Please start nvim and run ':UpdateRemotePlugins' manually\n"
+}
+
+hash vim >/dev/null 2>&1 || {
+  env OH_MY_NEOVIM="$OH_MY_NEOVIM" OH_MY_NEOVIM_EXTENSIONS="$OH_MY_NEOVIM_EXTENSIONS" vim -c ':PlugUpdate' -c ':qa!' >/dev/null || {
+    printf "Error: Update plugins failed\n"
+    printf "Please start nvim and run ':PlugUpdate' manually\n"
+  }
 }
 
 printf "\n${GREEN}Neovim plugins are now installed!${NORMAL}\n"
